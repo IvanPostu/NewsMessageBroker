298db35e89eb5945f55899f2244f1af5d73afd32
IvanPostu <ipostu20000127@gmail.com>
2021-09-05 01:12:17 +0300

    Create multi module project with simple common config

---
+++ news-message-broker/sender-app/src/main/java/com/ivan/sender_app/App.java
@@ -0,0 +1,20 @@
+package com.ivan.sender_app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    private static Logger log = LoggerFactory.getLogger(App.class);
+
+    // static {
+    //     System.setProperty("logback.configurationFile", "logback.xml");
+    // }
+
+    public static void main(String[] args) {
+        log.info("Hell1o World!");
+    }
+}
